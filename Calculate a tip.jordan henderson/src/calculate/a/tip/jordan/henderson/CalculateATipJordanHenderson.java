d245fec57656cfa6c2cfa6c01834aa6f886e8a86
Frost <frost@domo.com>
2014-06-30 22:54:40 -0600

    Looked at the Tip Calculator with Jordan. Made some changes.

--- Calculate a tip.jordan henderson/src/calculate/a/tip/jordan/henderson/CalculateATipJordanHenderson.java
+++ Calculate a tip.jordan henderson/src/calculate/a/tip/jordan/henderson/CalculateATipJordanHenderson.java
@@ -3,19 +3,26 @@ import java.util.Scanner;
 
 public class CalculateATipJordanHenderson
 {
-    public static void main(String[] args)
-    {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("11.50");
-        double bill = input.nextDouble();
-        
-        double Tip = bill * 0.18;
-        System.out.println("tip is 0.18" + (int)(bill * 0.18));
-        
-        
-        
-        
-    }
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+
+    //Prompt for Bill
+    System.out.println("Please enter the total for your bill.");
+    double bill = input.nextDouble();
+
+    //Prompt for desired tip percent
+    System.out.println("Please enter your tip percent. Example: for 15% tip, enter \"15\".");
+    double tipPercent = input.nextDouble() / 100;
+
+    //Calc the tip
+    double tip = bill * tipPercent;
+
+    //Calc the total bill
+    double totalBill = bill + tip;
+
+    //Show the total bill
+    System.out.println("Your total bill is: " + totalBill);
+  }
     
 }
